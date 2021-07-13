4a02753188f9588f9fcc02dfeb3c3e12421a1ca3
Orangeat <nsorangepv@gmail.com>
2021-07-13 19:43:36 +0900

    Update consent.js
    for printing request to 事務局.

--- Tシャツ処理/consent.js
+++ Tシャツ処理/consent.js
@@ -5,25 +5,50 @@ function onOpen() {
     .addToUi();
 }
 
-function createNewDocument() {
+function deleteMenu() {
     const spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
-    const sheet       = spreadsheet.getSheetByName('changes._同意差込');
-    const values      = sheet.getDataRange().getDisplayValues(); 
-    for (let i = 2; i < values.length; i++) {
-        replaceDocument(values[i]);
-    }
+    spreadsheet.removeMenu('追加メニュー');
 }
 
-function replaceDocument(array) {
-    const fileName          = String(array[0]);
-    const parentFolder      = DriveApp.getFolderById(DriveApp.getFileById(ss.getId()).getParents().next().getId());
-    const sourceDocument    = parentFolder.getFoldersByName('同意書テンプレート').next();
-    const copyDir           = parentFolder.getFoldersByName('同意書').next();
-    const duplicateDocument = sourceDocument.makeCopy(fileName, copyDir);
-    const targetDocument    = DocumentApp.openById(duplicateDocument.getId());
-    const targetBody        = targetDocument.getBody();
-    
-    for (let i = 0; i <= 28; i++) {
-        targetBody.replaceText('c' + String(i) + 'c', array[i]);
+function createNewDocument() {
+    const spreadsheet    = SpreadsheetApp.getActiveSpreadsheet();
+    const parentFolder   = DriveApp.getFolderById(DriveApp.getFileById(ss.getId()).getParents().next().getId());
+    const copyDir        = parentFolder.getFoldersByName('同意書').next();
+    const sheet          = spreadsheet.getSheetByName('changes._同意差込');
+    const values         = sheet.getDataRange().getDisplayValues(); 
+    const sourceDocument = parentFolder.getFilesByName('同意書テンプレート').next();
+
+    var iterList = copyDir.getFiles();
+    while (iterList.hasNext()) {
+        iterList.next().setTrashed(true);
+    }
+
+    for (array of values.slice(2)) {
+        const fileName = String(array[0]);
+        const duplicateDocument = sourceDocument.makeCopy(fileName, copyDir);
+        const ddId = duplicateDocument.getId();
+        const targetDocument = DocumentApp.openById(ddId);
+        const targetBody = targetDocument.getBody();
+
+        for (let i = 0; i <= 28; i++) {
+            targetBody.replaceText('c' + String(i) + 'c', array[i]);
+        }
+
+        Utilities.sleep(6000);
+        targetDocument.saveAndClose();
+
+        let token = ScirptApp.getOAuthToken();
+        let options = {
+            headers: {
+                'Authorization': 'Bearer' + token
+            },
+            muteHttpExceptions: true
+        };
+        let url = "https://docs.google.com/document/d/"
+                + ddId
+                + "/export?&exportFormat=pdf&format=pdf";
+        let blob = UrlFetchApp.fetch(url, options).getBlob().setName(fileName + '.pdf');
+        copyDir.createFile(blob);
+        targetDocument.setTrashed(true);
     }
 }
---
+++ Tシャツ処理/not_yet_all_print.js
@@ -0,0 +1,50 @@
+function not_yet_all_print() {
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    const sheet = ss.getSheetByName('未提出者印刷');
+    const ssID = ss.getId();
+    const shID = sheet.getSheetId();
+    const parentFolders = DriveApp.getFileById(ss.getId()).getParents();
+    const folder = DriveApp.getFolderById(parentFolders.next().getId()).getFoldersByName('未提出者').next();
+    let baseUrl = "https://docs.google.com/spreadsheets/d/"
+        + ssID
+        + "/export?gid="
+        + shID;
+    let pdfOptions = "&exportFormat=pdf&format=pdf"
+        + "&size=A4"
+        + '&portrait=true'
+        + "&fitw=true"
+        + "&top_margin=0.1"
+        + "&bottom_margin=0.1"
+        + "&left_margin=0.1"
+        + "&right_margin=0.1"
+        + "&horizontal_alignment=LEFT"
+        + "&vertical_alignment=TOP"
+        + "&gridlines=false";
+    let url = baseUrl + pdfOptions;
+    const cartesian =
+        (...a) => a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())));
+    let classes = cartesian(["J1", "J2", "J3", "S1", "S2"], ["A", "B", "C", "D", "E"]);
+    classes.push(["S2", "F"]);
+    var iterList = folder.getFiles();
+    while (iterList.hasNext()) {
+        iterList.next().setTrashed(true);
+    }
+    for (let c of classes) {
+        let grd, cls;
+        [grd, cls] = c;
+        const v = grd + cls;
+        let token = ScriptApp.getOAuthToken();
+        let options = {
+          headers: {
+              'Authorization': 'Bearer ' +  token
+          },
+          muteHttpExceptions : true
+        };
+        sheet.getRange("B2").setValue(v);
+        SpreadsheetApp.flush();
+        let blob = UrlFetchApp.fetch(url, options).getBlob().setName(v + '.pdf');
+        folder.createFile(blob);
+        Utilities.sleep(6000);
+    }
+    return true;
+}
