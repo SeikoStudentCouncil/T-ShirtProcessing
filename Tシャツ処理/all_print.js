798bae07301dbed47f48fec91bcec0813e0880ef
Orangeat <nsorangepv@gmail.com>
2021-07-22 23:56:36 +0900

    Create modifier.js

---
+++ Tシャツ処理/all_print.js
@@ -0,0 +1,63 @@
+function all_print() {
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    const intro = ss.getSheetByName('Intro.');
+    const trigger = intro.getRange("H19");
+    
+    if (trigger.getValue() == false) {
+        return false;
+    }
+    
+    const sheet = ss.getSheetByName('掲示印刷');
+    const ssID = ss.getId();
+    const shID = sheet.getSheetId();
+    const parentFolders = DriveApp.getFileById(ss.getId()).getParents();
+    const folder = DriveApp.getFolderById(parentFolders.next().getId()).getFoldersByName('pdf').next();
+    let baseUrl = "https://docs.google.com/spreadsheets/d/"
+        + ssID
+        + "/export?gid="
+        + shID;
+    let pdfOptions = "&exportFormat=pdf&format=pdf"
+        + "&size=A4"
+        + "&portrait=false"
+        + "&fitw=true"
+        + "&top_margin=0.1"
+        + "&bottom_margin=0.1"
+        + "&left_margin=0.1"
+        + "&right_margin=0.1"
+        + "&horizontal_alignment=LEFT"
+        + "&vertical_alignment=TOP"
+        + "&gridlines=false";
+    let url = baseUrl + pdfOptions;
+    const cartesian =
+        (...a) => a.reduce((a, b) => a.flatMap(d => b.map(e => [d, e].flat())));
+
+    let classes = cartesian(["J1", "J2", "J3", "S1", "S2"], ["A", "B", "C", "D", "E"]);
+
+    classes.push(["S2", "F"]);
+    
+    trigger.setValue(false);
+    
+    var iterList = folder.getFiles();
+    while (iterList.hasNext()) {
+        iterList.next().setTrashed(true);
+    }
+    for (let c of classes) {
+        let grd, cls;
+        [grd, cls] = c;
+        const v = grd + cls;
+        let token = ScriptApp.getOAuthToken();
+        let options = {
+          headers: {
+              'Authorization': 'Bearer ' +  token
+          },
+          muteHttpExceptions : true
+        };
+        sheet.getRange("B2").setValue(v);
+        SpreadsheetApp.flush();
+        let blob = UrlFetchApp.fetch(url, options).getBlob().setName(v + '.pdf');
+        folder.createFile(blob);
+        Utilities.sleep(6000);
+    }
+
+    return true;
+}
---
+++ Tシャツ処理/modifier.js
@@ -0,0 +1,42 @@
+function modifier() {
+    const ss = SpreadsheetApp.getActiveSpreadsheet();
+    const diff = ss.getSheetByName('diff.');
+    const change = ss.getSheetByName('changes.');
+/*
+    const trigger = change.getRange("H1");
+    if (trigger.getValue() == false) {
+        return false;
+    }
+    trigger.setValue(false);
+*/
+    let data1 = diff.getRange(3, 1, getDataLastRow(diff) - 1, 26).getValues()
+                .map((element) => element.slice(0, 5).concat(element.slice(6)));
+    let data2 = change.getRange(3, 5, getDataLastRow(change) - 1, 28).getValues()
+                .filter((element) => !/^B-/.test(element[1]));
+
+    data2.sort((a, b) => {
+        if (a[3] < b[3]) return -1;
+        if (a[3] > b[3]) return 1;
+    });
+
+    let j = 0;
+    let r = getDataLastRow(change) + 2;
+    for (let i = 0; i < data1.length; i++) {
+        if (j >= data2.length || data1[i][0] != data2[j][3]) {
+            change.getRange(r, 8, 1, 25).setValues([data1[i]]);
+            r++;
+        } else if (JSON.stringify(data1[i]) != JSON.stringify(data2[j].slice(3))) {
+            change.getRange(data2[j][0] + 2, 8, 1, 25).setValues([data1[i]]);
+            req = change.getRange(data2[j][0] + 2, 4)
+            req.setValue(String(req.getValue()) + ', modified')
+            j++;
+        } else {
+            j++;
+        }
+    }
+    return true;
+}
+
+function getDataLastRow(sheet) {
+    return sheet.getRange('B:B').getValues().filter(String).length;
+}
